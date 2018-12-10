a9f9fc1550a788e5fd08dc2700f703e656ff48e1
JesseleDurana <jesseleadt@gmail.com>
2018-12-09 23:28:27 -0400

    start modal per activo

--- src/components/organisms/ActivoModal.js
+++ src/components/organisms/ActivoModal.js
@@ -4,6 +4,13 @@ import Modal from "@material-ui/core/Modal";
 import Paper from "@material-ui/core/Paper";
 import ActivosForm from "./ActivosForm";
 
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import withMobileDialog from '@material-ui/core/withMobileDialog';
 
 class ActivoModal extends React.Component {
     state = {};
@@ -12,21 +19,36 @@ class ActivoModal extends React.Component {
         const {
             activo,
             open,
+            close
         } = this.props;
         return (
-            <Modal open={open}>
-                <Paper>
-                    {activo &&
-                    <ActivosForm
-                        activo={activo}
-                        clasificaciones={[activo.clasificacion]}
-                        marcas={[activo.marca]}
-                        ubicaciones={[{ desubifis: activo.desubifis, codubifis: activo.desubifis }]}
-                        disabled
-                    />
-                    }
-                </Paper>
-            </Modal>
+            <Dialog
+                open={open}
+                onClose={close}
+                aria-labelledby="responsive-dialog-title"
+            >
+                <DialogTitle id="responsive-dialog-title">{"Activo #"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                    Let Google help apps determine location. This means sending anonymous location data to
+                    Google, even when no apps are running.
+                    </DialogContentText>
+                        {activo &&
+                            <ActivosForm
+                                activo={activo}
+                                clasificaciones={[activo.clasificacion]}
+                                marcas={[activo.marca]}
+                                ubicaciones={[{ desubifis: activo.desubifis, codubifis: activo.desubifis }]}
+                                disabled
+                            />
+                        }
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={close} color="primary" autoFocus>
+                    Cerrar
+                    </Button>
+                </DialogActions>
+            </Dialog>
         );
     }
 }
