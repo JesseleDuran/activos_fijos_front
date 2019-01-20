03f0556aa5ef1354168971aa72c22adb7a465ebe
JesseleDurana <jesseleadt@gmail.com>
2019-01-20 14:47:49 -0400

    delete unused files

--- src/components/atoms/ChoppedText.js
+++ src/components/atoms/ChoppedText.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const ChoppedText = ({ text, length }) =>
   text.length > length ? `${text.substring(0, length - 3)}...` : text;
 
--- src/components/organisms/ActivoModal.js
+++ src/components/organisms/ActivoModal.js
@@ -1,16 +1,11 @@
 import React from "react";
-
-import Modal from "@material-ui/core/Modal";
-import Paper from "@material-ui/core/Paper";
 import ActivosForm from "./ActivosForm";
-
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import withMobileDialog from '@material-ui/core/withMobileDialog';
 
 class ActivoModal extends React.Component {
     state = {};
