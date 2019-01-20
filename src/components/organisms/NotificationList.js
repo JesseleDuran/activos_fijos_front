067f60d35a959ddaa5e585f41367877a2f38b7d6
JesseleDurana <jesseleadt@gmail.com>
2019-01-20 12:56:51 -0400

    feat: show notifications

--- src/components/organisms/NotificationList.js
+++ src/components/organisms/NotificationList.js
@@ -6,8 +6,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
+import Warning from '@material-ui/icons/Warning';
 
 const styles = theme => ({
   root: {
@@ -15,31 +14,33 @@ const styles = theme => ({
   },
 });
 
-function ListItemLink(props) {
-    return <ListItem button component="a" {...props} />;
+function generateDetailLevelDropdown(notifications) {
+    return notifications.map((i, ind) => [
+        <ListItem button>
+                <ListItemIcon>
+                    <Warning />
+                </ListItemIcon>
+                <ListItemText 
+                    primary={`¡El activo ${i.data.n_activo} está a punto de terminar su vida útil! Le faltan ${i.data.vida_util_faltante_dias} días.`} />         
+        </ListItem>,
+        <Divider />
+    ])
 }
 
 function NotificationList(props) {
-  const { classes } = props;
-  return (
-    <div>
-        <List component="nav">
-            <ListItem button>
-            <ListItemIcon>
-                <InboxIcon />
-            </ListItemIcon>
-            <ListItemText primary="Inbox" />
-            </ListItem>
-            <ListItem button>
-            <ListItemIcon>
-                <DraftsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Drafts" />
-            </ListItem>
-        </List>
-        <Divider />
-    </div>
-  );
+    const { notifications } = props;
+    notifications.map(i => (
+        i.data = JSON.parse(i.data)
+    ))
+
+    return (
+        <div>
+            <List 
+                component="nav">
+                {generateDetailLevelDropdown(notifications)}
+            </List>
+        </div>
+    );
 }
 
 export default withStyles(styles)(NotificationList);
