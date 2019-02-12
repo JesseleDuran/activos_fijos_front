69237d1c040171ebfc3a18f8adb3ae5fcbb83a82
JesseleDurana <jesseleadt@gmail.com>
2019-02-11 22:43:42 -0400

    add new columns to depreciacion table

--- src/tableConfig/reportes.js
+++ src/tableConfig/reportes.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { displayDateRightFormat } from "../utils/dates"
 
 export default [
     // Follow React-Table Documentation https://react-table.js.org
     {
+        Header: "Ubicación",
+        columns: [
+            {
+                Header: "Geográfica",
+                accessor: "ubicacion_geografica",
+            },
+            {
+                Header: "Administrativa",
+                accessor: "unidad_administrativa",
+            },
+        ],
+    },
+    {
         Header: "Información General",
         columns: [
             {
-                Header: "#",
+                Header: "N°",
                 accessor: "n_activo",
             },
             {
@@ -24,6 +38,12 @@ export default [
             {
                 Header: "Descripción",
                 accessor: "descripcion",
+                width: 150
+            },
+            {
+                id: "created_at",
+                Header: "Incorporación al sistema",
+                accessor: created_at => displayDateRightFormat(created_at),
             },
             {
                 Header: "Vida Útil (Meses)",
@@ -33,6 +53,52 @@ export default [
             {
                 Header: "Clasificación",
                 accessor: "clasificacion",
+            }
+        ],
+    },
+    {
+        Header: "Datos de compra",
+        columns: [
+            {
+                Header: "Cuenta presupuestaria",
+                accessor: "cuenta_presupuestaria",
+            },
+            {
+                Header: "Centro de costo",
+                accessor: "centro_costo",
+            },
+            {
+                id: "fecha_compra",
+                Header: "Fecha de Compra",
+                accessor: fecha_compra => displayDateRightFormat(fecha_compra),
+            },
+            {
+                id: "fin_vida_util",
+                Header: "Fin de vida útil",
+                accessor: fin_vida_util => displayDateRightFormat(fin_vida_util),
+            },
+            {
+                Header: "N° Orden de Compra",
+                accessor: "numero_orden_compra",
+                width: 150
+            },
+            {
+                Header: "N° Factura",
+                accessor: "numero_factura",
+                width: 150
+            },
+            {
+                Header: "Costo",
+                accessor: "costo_unitario",
+            },
+            {
+                Header: "Condición de pago",
+                accessor: "condicion_pago",
+            },
+            {
+                Header: "Proveedor",
+                accessor: "nombre_proveedor",
+                width: 150
             },
         ],
     },
