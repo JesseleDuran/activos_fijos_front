c5bbe6d9b309f6feaa627c94e8cb0b7a2e0a2260
JesseleDurana <jesseleadt@gmail.com>
2019-03-28 18:03:56 -0400

    change position of descargar inventario button

--- src/components/pages/ActivosPage.js
+++ src/components/pages/ActivosPage.js
@@ -13,6 +13,25 @@ const styles = () => ({});
 const ActivosPage = ({ activos, size, pages = 100, loading = false, onChange, show, remove, activo, close, onChangeUpdate, update, hasChanged, clasificaciones, marcas, downloadCartaDeAsignacion }) => (
     <Grid container>
         <div style={{ width: "100%" }}>
+            <Workbook filename="inventario.xlsx" element={<Button style={{ padding: '10px', marginTop: '-45px', marginLeft: '1085px', position: 'absolute'}} variant="contained" color="primary" disabled={activos.length === 0}>Descargar Inventario</Button>}>
+                <Workbook.Sheet data={activos} name="Sheet A">
+                    <Workbook.Column label="UBICACIÓN GEOGRÁFICA" value="ubicacion_geografica"/>
+                    <Workbook.Column label="UBICACIÓN ADMINISTRATIVA" value="ubicacion_administrativa"/>
+                    <Workbook.Column label="UBICACIÓN DEPARTAMENTO" value="departamento"/>
+                    <Workbook.Column label="USUARIO" value={row => row.nombre_personal ? `${row.nombre_personal} ${row.apellido_personal}` : ''}/>
+                    <Workbook.Column label="CÉDULA DE USUARIO" value={row => row.cedula_personal !== null ? `V-${row.cedula_personal}` : ''}/>
+                    <Workbook.Column label="N° ACTIVO" value="n_activo"/>
+                    <Workbook.Column label="DESCRIPCIÓN" value="descripcion"/>
+                    <Workbook.Column label="MARCA" value="marca"/>
+                    <Workbook.Column label="MODELO" value="modelo"/>
+                    <Workbook.Column label="SERIAL" value="serial"/>
+                    <Workbook.Column label="ESTADO DE USO" value="condicion"/>
+                    <Workbook.Column label="VIDA ÚTIL (MESES)" value="vida_util_meses"/>
+                    <Workbook.Column label="CLASIFICACIÓN" value="clasificacion"/>
+
+                    <Workbook.Column label="OBSERVACIONES" value="observaciones"/>                  
+                </Workbook.Sheet>
+            </Workbook>
             <ReactTable
                 data={activos}
                 columns={tableConfig(show, remove)}
@@ -36,25 +55,7 @@ const ActivosPage = ({ activos, size, pages = 100, loading = false, onChange, sh
                 clasificaciones={clasificaciones}
                 downloadCartaDeAsignacion={downloadCartaDeAsignacion}
             />
-            <Workbook filename="inventario.xlsx" element={<Button style={{padding: "15px"}} variant="contained" color="primary" disabled={activos.length === 0}>Descargar Inventario</Button>}>
-                <Workbook.Sheet data={activos} name="Sheet A">
-                    <Workbook.Column label="UBICACIÓN GEOGRÁFICA" value="ubicacion_geografica"/>
-                    <Workbook.Column label="UBICACIÓN ADMINISTRATIVA" value="ubicacion_administrativa"/>
-                    <Workbook.Column label="UBICACIÓN DEPARTAMENTO" value="departamento"/>
-                    <Workbook.Column label="USUARIO" value={row => row.nombre_personal ? `${row.nombre_personal} ${row.apellido_personal}` : ''}/>
-                    <Workbook.Column label="CÉDULA DE USUARIO" value={row => row.cedula_personal !== null ? `V-${row.cedula_personal}` : ''}/>
-                    <Workbook.Column label="N° ACTIVO" value="n_activo"/>
-                    <Workbook.Column label="DESCRIPCIÓN" value="descripcion"/>
-                    <Workbook.Column label="MARCA" value="marca"/>
-                    <Workbook.Column label="MODELO" value="modelo"/>
-                    <Workbook.Column label="SERIAL" value="serial"/>
-                    <Workbook.Column label="ESTADO DE USO" value="condicion"/>
-                    <Workbook.Column label="VIDA ÚTIL (MESES)" value="vida_util_meses"/>
-                    <Workbook.Column label="CLASIFICACIÓN" value="clasificacion"/>
 
-                    <Workbook.Column label="OBSERVACIONES" value="observaciones"/>                  
-                </Workbook.Sheet>
-            </Workbook>
         </div>
     </Grid>
 );
